dfab03c5dd987daa3f1b72c0f91ddb01ba5ab192
sangun1234 <sangun5104@gmail.com>
2024-04-02 22:57:02 +0900

    architecture defined

--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "graphql-tools";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 // ** : 모든 폴더 안/  * : 모든 파일
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -10,28 +10,5 @@ export default gql`
         createAt: String!
         updateAt: String!
     }
-
-    type LoginResult{
-        ok:Boolean!
-        token: String
-        error: String
-    }
-
-    type Mutation {
-        createAccount(
-            firstName:String!
-            lastName:String
-            userName:String!
-            email:String!
-            password:String!
-        ) : User
-        login(
-            email:String!, password:String!
-        ) : LoginResult!
-    }
-
-    type Query {
-        seeProfile(userName:String): User
-    }
 `;
 
